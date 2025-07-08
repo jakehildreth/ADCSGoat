00a002108e66b712727e818a4b497450d53cce51
Jake Hildreth <93942157+TrimarcJake@users.noreply.github.com>
2025-07-07 21:25:54 -0400

    Initial commit.

---
+++ Build-LocksmithLab/Private/New-BLLBlankTemplateObject.ps1
@@ -0,0 +1,35 @@
+function New-BLLBlankTemplateObject {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline)]
+        # TODO Add input validation to $TemplateName and $name
+        [string[]]$TemplateName
+    )
+
+    begin {
+        # Load the S.DS
+        Add-Type -AssemblyName System.DirectoryServices
+
+        # Get the Configuration partition automatically via RootDSE
+        $RootDSE = New-Object System.DirectoryServices.DirectoryEntry("LDAP://RootDSE")
+        $ConfigurationPartition = $rootDSE.configurationNamingContext
+        $TemplatesContainer = "CN=Certificate Templates,CN=Public Key Services,CN=Services,$ConfigurationPartition"
+        $TemplatePath = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$TemplatesContainer")
+    }
+
+    process {
+        Write-Output $TemplateName -PipelineVariable name | ForEach-Object {
+            $success = $false
+            while (-not $success) {
+                try {
+                    $newTemplate = $TemplatePath.Children.Add("CN=$name", "pKICertificateTemplate")
+                    $newTemplate.CommitChanges()
+                    $success = $true
+                } catch {
+                    Write-Error "That template name ($name) is already is invalid. Please enter a new name."
+                    $name = Read-Host -Prompt 'New Template Name'
+                }
+            }
+        }
+    }
+}
