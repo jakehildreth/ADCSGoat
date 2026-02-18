91e7fe08b7eb85f42687e0df50082d08a379275d
Holger Zimmermann <96825160+HerrHozi@users.noreply.github.com>
2026-02-18 13:37:28 +0100

    Add files via upload

--- ADCSGoat/ADCSGoat.psm1
+++ ADCSGoat/ADCSGoat.psm1
@@ -1,10 +1,15 @@
+
+$ModulePath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$Script:ResearchFolder = Join-Path -Path $ModulePath -ChildPath "Research"
+
 # Dot source public/private functions
-$public  = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
+$public = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
 $private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
 foreach ($import in @($public + $private)) {
     try {
         . $import.FullName
-    } catch {
+    }
+    catch {
         throw "Unable to dot source [$($import.FullName)]"
     }
 }
