093d4dcff6fcf1e5686e7d4c2d380cae441d7bd8
Jake Hildreth <93942157+jakehildreth@users.noreply.github.com>
2026-02-24 21:07:29 -0500

    refactor: Bring Set-AGTemplateAce.ps1 inline with existing style

--- ADCSGoat/Public/Set-AGTemplateAce.ps1
+++ ADCSGoat/Public/Set-AGTemplateAce.ps1
@@ -12,51 +12,41 @@ function Set-AGTemplateAce {
     )
 
     begin {
-
+        # Load the S.DS
         Add-Type -AssemblyName System.DirectoryServices
 
-        # ============================================================
-        # Get Configuration Naming Context
-        # ============================================================
-
+        # Get the Configuration partition automatically via RootDSE
         try {
             $RootDSE = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$Server/RootDSE")
             $ConfigurationPartition = $RootDSE.configurationNamingContext
-        }
-        catch {
+        } catch {
             throw "Failed to query RootDSE on $Server. $_"
         }
-
         $TemplateContainer = "CN=Certificate Templates,CN=Public Key Services,CN=Services,$ConfigurationPartition"
 
-        # ============================================================
-        # Security Identifiers
-        # ============================================================
-
+        # Define principals for use in ACEs
+        # $Administrators = New-Object System.Security.Principal.SecurityIdentifier('S-1-5-32-544')
         $AuthenticatedUsers = New-Object System.Security.Principal.SecurityIdentifier('S-1-5-11')
 
-        # ============================================================
-        # AD Rights
-        # ============================================================
-
+        # Define Active Directory Rights for use in ACEs
         $ExtendedRight = [System.DirectoryServices.ActiveDirectoryRights]::ExtendedRight
         $GenericAll = [System.DirectoryServices.ActiveDirectoryRights]::GenericAll
         $GenericRead = [System.DirectoryServices.ActiveDirectoryRights]::GenericRead
+        # $ReadProperty = [System.DirectoryServices.ActiveDirectoryRights]::ReadProperty
+        # $WriteOwner = [System.DirectoryServices.ActiveDirectoryRights]::WriteOwner
         $WriteProperty = [System.DirectoryServices.ActiveDirectoryRights]::WriteProperty
 
-        # ============================================================
-        # GUIDs
-        # ============================================================
-
+        # Define GUIDs for use in ACEs
+        # TODO get the ms-PKI GIDs
         $AllPropertiesGUID = [GUID]'00000000-0000-0000-0000-000000000000'
         $EnrollGUID = [GUID]'0e10c968-78fb-11d2-90d4-00c04f79dc55'
+        # $AutoEnrollGUID    = [GUID]'a05b8cc2-17bc-4802-a710-e7c15ab866a2'
 
+        # Define Access Control Type for use in ACEs
         $Allow = [System.Security.AccessControl.AccessControlType]::Allow
+        # $Deny = [System.Security.AccessControl.AccessControlType]::Deny
 
-        # ============================================================
         # Build AccessRule
-        # ============================================================
-
         $AccessRule = switch ($AceType) {
 
             'Enroll' {
@@ -87,38 +77,31 @@ function Set-AGTemplateAce {
     }
 
     process {
+        Write-Output $TemplateName -PipelineVariable name | ForEach-Object {
+            $success = $false
+            $TemplateObject = New-Object System.DirectoryServices.DirectoryEntry("LDAP://CN=$name,$TemplateContainer")
 
-        foreach ($name in $TemplateName) {
-
-            Write-Verbose "Processing template: $name"
-
-            $TemplateObject = New-Object System.DirectoryServices.DirectoryEntry(
-                "LDAP://$Server/CN=$name,$TemplateContainer"
-            )
-
-            try {
-                $ACL = $TemplateObject.ObjectSecurity
-            }
-            catch {
-                throw "Could not read ACL from template '$name' on $pdc. $_"
-            }
-
-            foreach ($ace in $AccessRule) {
-                $ACL.AddAccessRule($ace)
-            }
-
-            try {
-                $TemplateObject.ObjectSecurity = $ACL
-                $TemplateObject.CommitChanges()
-                Write-Verbose "Successfully updated template '$name' on $pdc"
-            }
-            catch {
-                $msg = $_.Exception.Message
-                if ($_.Exception.InnerException) {
-                    $msg += "`nInnerException: $($_.Exception.InnerException.Message)"
+            while (-not $success) {
+                # Get the current ACL
+                $ACL = try {
+                    $TemplateObject.ObjectSecurity
+                } catch {
+                    throw "Could not collect ACL from $name (CN=$name,$TemplateContainer). Do you have rights to read the template object?"
                 }
 
-                throw "Failed to apply ACL to template '$name' on $pdc.`n$msg"
+                # Add each access rule to the ACL
+                Write-Output $AccessRule -PipelineVariable ace | ForEach-Object {
+                    $ACL.AddAccessRule($ace)
+                }
+
+                try {
+                    $TemplateObject.ObjectSecurity = $ACL
+                    $TemplateObject.CommitChanges()
+                    $success = $true
+                } catch {
+                    throw "Could not apply new ACL to $name (CN=$name,$TemplateContainer). Do you have rights to write to the template object?"
+                    # exit
+                }
             }
         }
     }
